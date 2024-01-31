4911685fa1ebbec8058ddff14f5b024cc7005dc4
Karl Nicholas <karl.nicholas@outlook.com>
2024-01-31 11:07:51 -0500

    Updates.

--- src/main/java/org/example/auth0webclientaudience/SecurityConfiguration.java
+++ src/main/java/org/example/auth0webclientaudience/SecurityConfiguration.java
@@ -11,6 +11,9 @@ import org.springframework.security.web.SecurityFilterChain;
 
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            return http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
+            return http.authorizeHttpRequests(auth -> auth
+                            .anyRequest()
+                            .authenticated())
+                    .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
         }
     }
